bb6f2832ee19e1c2108627f0964b6632264d55ce
Dota43ver <facundoagueroc@gmail.com>
2024-06-04 19:51:22 -0300

    auth UI 5108

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { use, useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
 interface ModalProps {
   isOpen?: boolean;
   onClose: () => void;
@@ -5,10 +10,77 @@ interface ModalProps {
   title?: string;
   body?: React.ReactElement;
   footer?: React.ReactElement;
+  actionLabel?: string;
+  disabled?: boolean;
+  secodaryAction?: () => void;
+  secondaryLabel?: string;
 }
 
-const Modal = () => {
-  return <div></div>;
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secodaryAction,
+  secondaryLabel,
+}) => {
+  const [showModal, setShowModal] = useState(isOpen);
+
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secodaryAction) {
+      return;
+    }
+    secodaryAction();
+  }, [disabled, secodaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <>
+      <div className="justify-center items-center overflow-x-hidden overflow-y-auto flex fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+          <div
+            className={`translate duration-300 h-full ${
+              showModal ? "translate-y-0" : "translate-y-full"
+            } ${showModal ? "opacity-100" : "opacity-0"}`}>
+            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="flex items-center p-6 rounder-t justify-center relative botder-b-[1px]">
+                <button className="p-1 border-0 hover:opacity-70 transition absolute left-9">
+                  <IoMdClose size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
